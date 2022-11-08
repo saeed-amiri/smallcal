7774490a9dadc8f964c98e062858025068dc1682
Saeed Amiri <samiri@uni-bremen.de>
2022-11-08 12:23:29 +0100

    Molar mass constats

---
+++ src/constants.py
@@ -0,0 +1,10 @@
+class Doc:
+    """list of the constant, Molar mass or what else"""
+
+
+class MolarMass:
+    """Molar mass of the sample, they all are in gerams per mol"""
+    NaCl = 58.44
+    H2O = 18.01528 
+    Decane = 142.29
+    ODA = 269.509
